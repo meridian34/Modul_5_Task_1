9070822056820fcbeb808d7a1241715b6d606261
meridian34 <sobol.andrey.25@gmail.com>
2021-05-10 17:22:39 +0300

    Добавьте файлы проекта.

---
+++ Modul_5_Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul_5_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
