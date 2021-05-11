a28dfb16f03dee2d1d5799472ac21c78e921fbc0
Sobol Andrey <sobol.andrey.25@gmail.com>
2021-05-11 12:38:03 +0300

    update extension

--- Modul_5_Task_1/Extensions/HttpClientServiceExtensions.cs
+++ Modul_5_Task_1/Extensions/HttpClientServiceExtensions.cs
@@ -15,165 +15,95 @@ namespace Modul_5_Task_1.Extensions
 {
     public static class HttpClientServiceExtensions
     {
-        #region GET
-        public static async Task<Page<IReadOnlyCollection<User>>> GetUserCollectionAsync(this IHttpClientService httpClientService, string request)
+        public static async Task<R> GenHttpMethodAsync<P, R>(this IHttpClientService httpClientService, string uri, HttpMethod httpMethod, P dto = null)
+            where P: class
         {
-            var result = await httpClientService.GetAsync(request);
-            if(result.StatusCode == HttpStatusCode.OK)
+            
+            HttpResponseMessage result;
+            
+            if(dto is null)
             {
-                var content = await result.Content.ReadAsStringAsync();
-                var page = JsonConvert.DeserializeObject<Page<IReadOnlyCollection<User>>>(content);
-                return page;              
+                result = await httpClientService.SendAsync(uri, httpMethod);
             }
             else
             {
-                throw new HttpRequestException("Bad StatusCode");
-            }
-        }
-        public static async Task<Page<User>> GetUserAsync(this IHttpClientService httpClientService, string request)
-        {
-            var result = await httpClientService.GetAsync(request);
-            if (result.StatusCode == HttpStatusCode.OK)
-            {
-                var content = await result.Content.ReadAsStringAsync();
-                var page = JsonConvert.DeserializeObject<Page<User>>(content);
-                return page;
-            }
-            else
-            {
-                throw new HttpRequestException("Bad StatusCode");
-            }
-        }
-        public static async Task<Page<Resource>> GetResourceAsync(this IHttpClientService httpClientService, string request)
-        {
-            var result = await httpClientService.GetAsync(request);
-            if (result.StatusCode == HttpStatusCode.OK)
-            {
-                var content = await result.Content.ReadAsStringAsync();
-                var page = JsonConvert.DeserializeObject<Page<Resource>>(content);
-                return page;
-            }
-            else
-            {
-                throw new HttpRequestException("Bad StatusCode");
-            }
-        }
-        public static async Task<Page<IReadOnlyCollection<Resource>>> GetResourceCollectionAsync(this IHttpClientService httpClientService, string request)
-        {
-            var result = await httpClientService.GetAsync(request);
-            if (result.StatusCode == HttpStatusCode.OK)
-            {
-                var content = await result.Content.ReadAsStringAsync();
-                var page = JsonConvert.DeserializeObject<Page<IReadOnlyCollection<Resource>>>(content);
-                return page;
-            }
-            else
-            {
-                throw new HttpRequestException("Bad StatusCode");
-            }
-        }
-        #endregion
-        #region POST
-        public static async Task<User> CreateUserAsync(this IHttpClientService httpClientService, string uri, User user)
-        {
-            var serializeUser = JsonConvert.SerializeObject(user, Formatting.Indented, new JsonSerializerSettings 
-            {
-                NullValueHandling = NullValueHandling.Ignore
-            });
-            var result = await httpClientService.PostAsync(uri,serializeUser);
+                var serializeDTO = SerializeService.Serialize<P>(dto);
+                result = await httpClientService.SendAsync(uri, httpMethod, serializeDTO);
+            }                
+             
             if (result.StatusCode == HttpStatusCode.OK || result.StatusCode == HttpStatusCode.Created)
             {
                 var content = await result.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<User>(content);
+                return JsonConvert.DeserializeObject<R>(content);
+            }
+            else if(result.StatusCode == HttpStatusCode.NoContent)
+            {
+                return default(R);
             }
             else
             {
                 throw new HttpRequestException("Bad StatusCode");
             }
+        }
+
+        #region GET
+        public static async Task<T> GetDTOAsync<T>(this IHttpClientService httpClientService, string request)
+            where T: class
+        {
+            return await GenHttpMethodAsync<T, T>(httpClientService, request, HttpMethod.Get);
+        }
+        public static async Task<Page<IReadOnlyCollection<User>>> GetUserCollectionAsync(this IHttpClientService httpClientService, string request)
+        {
+            return await GetDTOAsync<Page<IReadOnlyCollection<User>>>(httpClientService, request);            
+        }
+        public static async Task<Page<User>> GetUserAsync(this IHttpClientService httpClientService, string request)
+        {
+            return await GetDTOAsync<Page<User>>(httpClientService, request);
+        }
+        public static async Task<Page<Resource>> GetResourceAsync(this IHttpClientService httpClientService, string request)
+        {
+            return await GetDTOAsync<Page<Resource>>(httpClientService, request);
+        }
+        public static async Task<Page<IReadOnlyCollection<Resource>>> GetResourceCollectionAsync(this IHttpClientService httpClientService, string request)
+        {
+            return await GetDTOAsync<Page<IReadOnlyCollection<Resource>>>(httpClientService, request);
+        }
+        #endregion
+        #region POST
+        public static async Task<R> PostDTOAsync<P,R>(this IHttpClientService httpClientService, string uri, P dto)
+            where P:class
+        {
+            return await GenHttpMethodAsync<P,R>(httpClientService, uri, HttpMethod.Post, dto);
+        }
+        public static async Task<User> CreateUserAsync(this IHttpClientService httpClientService, string uri, User user)
+        {
+            return await PostDTOAsync<User, User>(httpClientService, uri, user);
         }
         public static async Task<RegisterResponse> UserRegistrationAsync(this IHttpClientService httpClientService, string uri, RegistrationInfo registrationInfo)
         {
-            var serializeUser = JsonConvert.SerializeObject(registrationInfo, Formatting.Indented, new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore
-            });
-            var result = await httpClientService.PostAsync(uri, serializeUser);
-            if (result.StatusCode == HttpStatusCode.OK)
-            {
-                var content = await result.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<RegisterResponse>(content);
-            }
-            else
-            {
-                throw new HttpRequestException("Bad StatusCode");
-            }
+            return await PostDTOAsync<RegistrationInfo, RegisterResponse>(httpClientService, uri, registrationInfo);
         }
         public static async Task<RegisterResponse> LoginUserAsync(this IHttpClientService httpClientService, string uri, RegistrationInfo registrationInfo)
-        {   
-            var serializeUser = JsonConvert.SerializeObject(registrationInfo, Formatting.Indented, new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore
-            });
-            var result = await httpClientService.PostAsync(uri, serializeUser);
-            if (result.StatusCode == HttpStatusCode.OK)
-            {
-                var content = await result.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<RegisterResponse>(content);
-            }
-            else
-            {
-                throw new HttpRequestException("Bad StatusCode");
-            }
+        {
+            return await PostDTOAsync<RegistrationInfo, RegisterResponse>(httpClientService, uri, registrationInfo);
         }
         #endregion
         #region PUT
         public static async Task<User> UpdateUserAsync(this IHttpClientService httpClientService, string uri, User user)
         {
-            var serializeUser = JsonConvert.SerializeObject(user, Formatting.Indented, new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore
-            });
-            var result = await httpClientService.PutAsync(uri, serializeUser);
-            if (result.StatusCode == HttpStatusCode.OK)
-            {
-                var content = await result.Content.ReadAsStringAsync();                
-                return JsonConvert.DeserializeObject<User>(content);
-            }
-            else
-            {
-                throw new HttpRequestException("Bad StatusCode");
-            }
+            return await GenHttpMethodAsync<User, User>(httpClientService, uri, HttpMethod.Put, user);
         }
         #endregion
         #region PATCH
         public static async Task<User> UpdateUserViaPatchAsync(this IHttpClientService httpClientService, string uri, User user)
         {
-            var serializeUser = JsonConvert.SerializeObject(user, Formatting.Indented, new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore
-            });
-
-            var result = await httpClientService.PatchAsync(uri, serializeUser);
-            if (result.StatusCode == HttpStatusCode.OK)
-            {
-                var content = await result.Content.ReadAsStringAsync();
-                var page = JsonConvert.DeserializeObject<User>(content);
-                return page;
-            }
-            else
-            {
-                throw new HttpRequestException("Bad StatusCode");
-            }
+            return await GenHttpMethodAsync<User, User>(httpClientService, uri, HttpMethod.Patch, user);
         }
         #endregion
         #region DELETE
         public static async Task DeleteUserAsync(this IHttpClientService httpClientService, string request)
         {
-            var result = await httpClientService.GetAsync(request);
-            if (result.StatusCode != HttpStatusCode.OK)
-            {
-                throw new HttpRequestException("Bad StatusCode");
-            }
+            await GenHttpMethodAsync<object, object>(httpClientService, request, HttpMethod.Delete);
         }
         #endregion
     }
---
+++ Modul_5_Task_1/Services/SerializeService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_5_Task_1.Services
+{
+    class SerializeService
+    {
+        public static string Serialize<T>(T dto)
+        {
+            var serializeDTO = JsonConvert.SerializeObject(dto, Formatting.Indented, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            return serializeDTO;
+        }
+        public static T Deserialize<T>(string content)
+        {
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+    }
+}
